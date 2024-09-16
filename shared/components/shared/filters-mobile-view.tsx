46df0a5eae37ca4667508fc41d5683f438e88b30
dmitrijs <petja314@gmail.com>
2024-09-16 21:24:52 +0100

    filter / overflow-y-auto mobile verstion

--- shared/components/shared/filters-mobile-view.tsx
+++ shared/components/shared/filters-mobile-view.tsx
@@ -26,7 +26,7 @@ export const FiltersMobileView: React.FC<React.PropsWithChildren<Props>> = ({
                'flex flex-col justify-between pb-0 bg-[#F4F1EE] w-full '
             }
          >
-            <Filters className={'flex-1 scrollbar pb-5'} />
+            <Filters className={'overflow-y-auto flex-1 scrollbar pb-5'} />
          </SheetContent>
       </Sheet>
    );
